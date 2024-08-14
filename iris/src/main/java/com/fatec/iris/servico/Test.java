64e5761e1b71b4ee685d0a53d8019ae86be9f612
almeida1 <engsoftware1@gmail.com>
2024-08-14 17:25:14 -0300

    Versao inicial - rede neural - multilayer perceptron

---
+++ iris/src/main/java/com/fatec/iris/servico/Test.java
@@ -0,0 +1,54 @@
+package com.fatec.iris.servico;
+
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Debug;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Normalize;
+
+
+public class Test {
+
+   // public static final String DATASETPATH = "/Users/Emaraic/Temp/ml/iris.2D.arff";
+    public static final String DATASETPATH = "E:/data_set/iris_dataset.arff";
+   // public static final String MODElPATH = "/Users/Emaraic/Temp/ml/model.bin";
+    public static final String MODElPATH = "E:/data_set/model.bin/";
+    public static void main(String[] args) throws Exception {
+        
+        GeradorDoModelo mg = new GeradorDoModelo();
+
+        Instances dataset = mg.loadDataset(DATASETPATH);
+
+        Filter filter = new Normalize();
+
+        // divide dataset to train dataset 80% and test dataset 20%
+        int trainSize = (int) Math.round(dataset.numInstances() * 0.8);
+        int testSize = dataset.numInstances() - trainSize;
+
+        dataset.randomize(new Debug.Random(1));// if you comment this line the accuracy of the model will be droped from 96.6% to 80%
+        
+        //Normalize dataset
+        filter.setInputFormat(dataset);
+        Instances datasetnor = Filter.useFilter(dataset, filter);
+
+        Instances traindataset = new Instances(datasetnor, 0, trainSize);
+        Instances testdataset = new Instances(datasetnor, trainSize, testSize);
+
+        // build classifier with train dataset             
+        MultilayerPerceptron ann = (MultilayerPerceptron) mg.buildClassifier(traindataset);
+
+        // Evaluate classifier with test dataset
+        String evalsummary = mg.evaluateModel(ann, traindataset, testdataset);
+        System.out.println("Evaluation: " + evalsummary);
+
+        //Save model 
+        mg.saveModel(ann, MODElPATH);
+
+        //classifiy a single instance 
+        Classificacao cls = new Classificacao();
+        String classname =cls.classifiy(Filter.useFilter(cls.createInstance(1.6, 0.2, 0), filter), MODElPATH);
+        System.out.println("\n The class name for the instance with petallength = 1.6 and petalwidth =0.2 is  " +classname);
+
+    }
+
+}
